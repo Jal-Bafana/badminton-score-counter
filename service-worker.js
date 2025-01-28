0d0f2d82c68a88597cd3fa499e53e5b26059cbc9
Arjun Mehta <arjunmschool@gmail.com>
2025-01-28 21:17:31 +0530

    Service worker registration in script.js

--- service-worker.js
+++ service-worker.js
@@ -22,3 +22,11 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => console.log('ServiceWorker registered'))
+      .catch(err => console.log('ServiceWorker registration failed:', err));
+  });
+}
